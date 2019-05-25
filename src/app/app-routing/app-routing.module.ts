ccd5f51b01f82fd5efad9707c59b1da5592678c4
Linus <linusfrotscher@gmail.com>
2019-05-25 19:19:29 +0200

    new Club card created, routerlink working, fix up inputs for club creation next

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AuthGuard } from '../auth/auth-guard.service';
 import { HomeComponent } from '../home/home.component';
 import { DetailedViewComponent } from '../home/detailed-view/detailed-view.component';
 import { ProfilePageComponent } from '../home/profile-page/profile-page.component';
+import { CreateClubComponent } from '../home/create-club/create-club.component';
 
 const routes: Routes = [
   {
@@ -13,6 +14,10 @@ const routes: Routes = [
   {
     path: 'detailedClub/:id',
     component: DetailedViewComponent
+  },
+  {
+    path: 'createClub',
+    component: CreateClubComponent
   },
   {
     path: 'profile',
