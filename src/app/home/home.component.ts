8bde33a9a98951389f0b5e914e5f4bfa608a250c
Linus <linusfrotscher@gmail.com>
2019-05-26 12:09:29 +0200

    merged frontend into staging nad started cloudinary service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,24 +16,25 @@ export class HomeComponent implements OnInit {
     private imageService: ImageService
   ) {}
 
-  selectedFile;
+  // selectedFile;
+
   clubs;
 
-  processFile(imageInput: any) {
-    const file: File = imageInput.files[0];
-    const reader = new FileReader();
+  // processFile(imageInput: any) {
+  //   const file: File = imageInput.files[0];
+  //   const reader = new FileReader();
 
-    reader.addEventListener('load', (event: any) => {
-      this.selectedFile = new ImageSnippet(event.target.result, file);
+  //   reader.addEventListener('load', (event: any) => {
+  //     this.selectedFile = new ImageSnippet(event.target.result, file);
 
-      console.log(this.selectedFile.src);
-      this.imageService
-        .uploadImage(this.selectedFile.src)
-        .subscribe(data => console.log(data));
-    });
+  //     console.log(this.selectedFile.src);
+  //     this.imageService
+  //       .uploadImage(this.selectedFile.src)
+  //       .subscribe(data => console.log(data));
+  //   });
 
-    reader.readAsDataURL(file);
-  }
+  //   reader.readAsDataURL(file);
+  // }
 
   ngOnInit() {
     // ----------- GET CLUBS -> WORKING! -----------------------
@@ -100,6 +101,6 @@ export class HomeComponent implements OnInit {
   }
 }
 
-class ImageSnippet {
-  constructor(public src: string, public file: File) {}
-}
+// class ImageSnippet {
+//   constructor(public src: string, public file: File) {}
+// }
--- src/app/services/image.service.ts
+++ src/app/services/image.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { cloudinary } from 'cloudinary';
+
+// let cloudinary = require('cloudinary').v2;
 
 @Injectable({
   providedIn: 'root'
@@ -8,14 +11,25 @@ import { HttpClient } from '@angular/common/http';
 export class ImageService {
   constructor(private http: HttpClient) {}
 
-  public uploadImage(image: File) {
-    const formData = new FormData();
+  public uploadImage() {
+    // parameter -> image: File
+    // const formData = new FormData();
 
-    console.log(image);
-    // formData.append('image', image);
+    // console.log(image);
+
+    const params = {
+      params: new HttpParams()
+        .append('cloud_name', 'dejvimnnm')
+        .append('api_key', '398537455597268')
+        .append('api_secret', 'KEueI7QcRDYEpXEe7Ky88iVaVtc')
+    };
 
     let url =
       'https://api.cloudinary.com/v1_1/dejvimnnm/image/upload?callback=foo';
+
+    cloudinary.uploader.upload('~/Downloads/test.jpg', function(error, result) {
+      console.log(result);
+    });
 
     // return this.http.post('/api/image/upload', image);
     return this.http.post(url, image);
