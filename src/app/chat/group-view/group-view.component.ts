2418f7ef496129adab5a528f55d820ef5996ed5c
Linus <linusfrotscher@gmail.com>
2019-06-04 09:46:36 +0200

    working again

--- src/app/chat/group-view/group-view.component.ts
+++ src/app/chat/group-view/group-view.component.ts
@@ -4,21 +4,20 @@ import { environment } from '../../../environments/environment';
 import { ChatLoginComponent } from '../chat-login/chat-login.component';
 import { CometChatApiService } from '../CometChatService/comet-chat-api.service';
 
-
 @Component({
   selector: 'app-group-view',
   templateUrl: './group-view.component.html',
   styleUrls: ['./group-view.component.scss']
 })
 export class GroupViewComponent implements OnInit, OnDestroy {
-  
   groupId: 'supergroup';
   messages = [];
   listenerId = 'Web_App_Listener_Group_ID';
 
-  
-
-  constructor(private chatService: CometChatService,private chatAuth: CometChatApiService) {}
+  constructor(
+    private chatService: CometChatService,
+    private chatAuth: CometChatApiService
+  ) {}
 
   currentUser() {
     return localStorage.activeUser;
@@ -27,10 +26,10 @@ export class GroupViewComponent implements OnInit, OnDestroy {
   ngOnInit() {
     // Users are already members of the group
     this.chatService.joinGroup(this.groupId);
-    this.chatService.login(this.currentUser(),environment.cometChat.apiKey); // <-- add getActiveUserService
+    this.chatService.login(this.currentUser(), environment.cometChat.apiKey); // <-- add getActiveUserService
     this.getMessages().then(_ => this.listenForMessages());
     this.chatAuth.createAuthToken(this.currentUser());
-    this.chatAuth.createUser(this.currentUser(),"name");
+    this.chatAuth.createUser(this.currentUser(), 'name');
   }
 
   sendMessage(message: string) {
