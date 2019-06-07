e38ee095f0c92fb9cfe4d9855ab9c571f54117e5
Katie <katie@Admins-MBP-2.fritz.box>
2019-06-07 19:49:55 +0200

    error: chat cannot read groupid in group view component

--- src/app/chat/group-view/group-view.component.ts
+++ src/app/chat/group-view/group-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, getDebugNode } from '@angular/core';
 import { CometChatService } from '../CometChatService/comet-chat.service';
 import { environment } from '../../../environments/environment';
 import { ChatLoginComponent } from '../chat-login/chat-login.component';
@@ -29,6 +29,10 @@ export class GroupViewComponent implements OnInit, OnDestroy {
     return localStorage.activeUser;
   }
 
+  // currentGroup(){
+  //   return localStorage.clubID;
+  // }
+
   ngOnInit() {
     // this.chatService.joinGroup(this.groupId);
 
@@ -38,18 +42,51 @@ export class GroupViewComponent implements OnInit, OnDestroy {
       this.user = user;
       this.fullname = this.user.fullname;
     })
-    console.log(`local storage - clubID : ${localStorage.getItem("clubID")}`)
+    //console.log(`local storage - clubID : ${localStorage.getItem("clubID")}`)
+
     this.groupId = localStorage.getItem("clubID");
-
     this.chatService.login(this.currentUser(), environment.cometChat.apiKey);
-    console.log(`current user is logged in: ${this.currentUser()}`);
-    
-    setTimeout(function () {
-      this.getMessages().then(data => this.listenForMessages()); // this needs to run async, as groupid needs to be read from localstorage first, which takes about 3ms
-    }, 400);
+    this.getMessages().then(data => this.listenForMessages());
 
+    //console.log(`current user is logged in: ${this.currentUser()}`);
+
+    // getMessages needs to run async, as groupid needs to be read from localstorage first, 
+    //which takes about 3ms. Hence we will use a Promise and wait for it to resolve, before we continue
+    //this.groupId = localStorage.getItem("clubID");
+    //this.getGroupId().then(data => this.getMessages()).then(data => this.listenForMessages());
+    
+    this.getMessages().then(data => this.listenForMessages());
+
+    // this.asyncLocalStorage.getItem('clubID')
+    //   .then(function (value) {
+    //    // this.groupId = value;
+    //     console.log('Value has been set to:', value);
+    //   });
 
   }
+
+  // asyncLocalStorage = {
+  //   getItem: async function (key) {
+  //     await Promise.resolve();
+  //     return localStorage.getItem(key);
+  //   }
+  // }
+
+  // // function containing Promise to get groupId for chat room to be able to get messages
+  // getGroupId() {
+  //   return new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       if (this.groupId !== undefined) {
+  //         resolve('groupId was set successfully');
+
+  //       } else {
+  //         reject('groupId is still undefinded')
+  //         console.log(`local storage - clubID : ${localStorage.getItem("clubID")}`)
+  //       }
+  //     }, 500);
+  //   })
+  // }
+
 
   sendMessage(message: string) {
     this.messages.push({
@@ -58,11 +95,13 @@ export class GroupViewComponent implements OnInit, OnDestroy {
       // sender: { uid: this.currentUser() }
     });
     this.chatService.sendMessage(this.groupId, message);
+   // this.chatService.sendMessage(this.currentGroup(), message);
     console.log('message', this.messages);
   }
 
   getMessages() {
     return this.chatService
+      //.getPreviousMessages(this.currentGroup())
       .getPreviousMessages(this.groupId)
       .then(messages => (this.messages = messages))
       .then(console.log, console.error);
