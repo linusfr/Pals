2409cdd73276ee45563bfdd0044acc773310f493
catharine98 <catharine.kraemer@t-online.de>
2019-06-09 23:18:48 +0200

    commits

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -10,18 +10,23 @@ router.use(passport.authenticate('jwt', { session: false }));
 
 router.route('/').post(asyncHandler(insert));
 
+//liefert den Services den aktiven Nutzer
 router.get('/activeUser', async (req, res) => {
   console.log('test', req.query.userID);
   let user = await userCtrl.getActiveUser(req.query.userID);
   res.json(user);
 });
 
+//leitet Änderungen am Nutzer weiter an passenden Controller
 router.post('/edit', async (req, res) => {
   let { id, fullName, userEmail } = req.body;
   let success = await userCtrl.editUser(id, fullName, userEmail);
   res.json(success);
 });
 
+
+//ruft die Funktion insert von dem User-controller auf 
+//und übergibt den req.Body als Parameter
 async function insert(req, res) {
   let user = await userCtrl.insert(req.body);
   res.json(user);
