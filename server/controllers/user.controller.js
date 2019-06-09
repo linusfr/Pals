786ffde2f33a341b8c7ae9b3a08943946f8c6ba5
Manuel Urban <40859767+Manuel-Urban@users.noreply.github.com>
2019-06-09 20:45:02 +0200

    so viele kommentare

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,3 +1,7 @@
+//----------------------------------------------------------------
+//Dieser Controller verwaltet alle Datenbankzugriffe für die User
+//----------------------------------------------------------------
+
 const bcrypt = require('bcrypt');
 const Joi = require('joi');
 const User = require('../models/user.model');
@@ -17,33 +21,41 @@ module.exports = {
   editUser
 };
 
+//Neuen User hinzufügen
 async function insert(user) {
   user = await Joi.validate(user, userSchema, { abortEarly: false });
+  //Hashwert vom Passwort generieren
   user.hashedPassword = bcrypt.hashSync(user.password, 10);
+  //Nach dem Generieren des Hashwertes wird das ursprüngliche Passwort gelöscht
   delete user.password;
+  //Der User wird eingespeichert
   return await new User(user).save();
 }
 
+//User bearbeiten
 async function editUser(id, fullName, userEmail) {
   let data;
+  //Schaut, ob es die E-Mail-Adresse schon gibt
   try {
     data = await User.find({ email: userEmail });
   } catch (e) {
     console.log(e);
   }
-
-  console.log(data, data.length);
+  //Falls die E-Mail-Adresse schon existiert
   if (data.length !== 0) {
     return 'emailExists';
   }
 
+  //Sucht den User anhand der ID
   let oldUser = await User.findOne({ _id: id });
   oldUser.fullname = fullName;
   oldUser.email = userEmail;
 
+  //ersetzt die alten Daten mit neuen Daten
   return await User.replaceOne({ _id: id }, oldUser);
 }
 
+//Gibt die Daten des aktuell angemeldeten Users zurück
 async function getActiveUser(userID) {
   let user = await User.find({ _id: userID });
 
